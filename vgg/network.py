9648e01439e7e42e71907a1246d2d12cabf90b9b
Junbao Chen <Chen.Junbao@outlook.com>
2020-05-16 21:26:43 +0800

    feat: add vgg

---
+++ vgg/network.py
@@ -0,0 +1,94 @@
+import torch.nn as nn
+
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc1 = nn.Linear(in_features=512, out_features=4096)
+        self.activation1 = nn.ReLU()
+        self.dropout1 = nn.Dropout()
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.activation2 = nn.ReLU()
+        self.dropout2 = nn.Dropout()
+        self.fc3 = nn.Linear(in_features=4096, out_features=10)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output = self.conv5(output)
+        output = output.reshape(output.size(0), -1)
+        output = self.fc1(output)
+        output = self.activation1(output)
+        output = self.dropout1(output)
+        output = self.fc2(output)
+        output = self.activation2(output)
+        output = self.dropout2(output)
+        output = self.fc3(output)
+
+        return output
---
+++ vgg/train.py
@@ -0,0 +1,68 @@
+from dataset.generate_dataset import *
+from vgg.network import VGG19
+
+import torch.nn as nn
+import torch.utils.data
+
+if __name__ == "__main__":
+
+    dataset = CifarTrain()
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=256)
+
+    dataset = CifarTest()
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=256)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    model = VGG19().cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    best_accuracy = 0.0
+
+    epoch = 100
+    for i in range(epoch):
+        print("epoch:", i)
+
+        model.train()
+
+        for j, data in enumerate(train_loader):
+            x, y = data
+            x = x.cuda()
+            y = y.cuda()
+
+            x_var = torch.autograd.Variable(x)
+
+            prediction = model(x_var)
+
+            loss = criterion(prediction, y.long())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        correct = torch.zeros(1).squeeze().cuda()
+        total = torch.zeros(1).squeeze().cuda()
+
+        model.eval()
+        with torch.no_grad():
+            for j, data in enumerate(test_loader):
+                x, y = data
+                x = x.cuda()
+                y = y.cuda()
+
+                x_var = torch.autograd.Variable(x)
+
+                output = model(x_var)
+
+                prediction = torch.argmax(output, 1)
+
+                correct += (prediction == y.long()).sum().float()
+                total += len(y)
+
+            accuracy = (correct / total).cpu().item()
+            print("accuracy:", accuracy)
+            if accuracy > best_accuracy:
+                # save best model
+                torch.save(model, "../model/vgg-19.pth")
+                best_accuracy = accuracy
